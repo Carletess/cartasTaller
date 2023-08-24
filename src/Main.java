9570971eaea4d488662d026ec80a4e8ce2490ee4
carlos <c.pradenas02@ufromail.cl>
2023-08-24 15:33:32 -0400

    crearMatrizCartas modificado

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,42 @@
-//a
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+
+
+
+    }
+    public static void crearMatrizCartas(){
+        String cartas[][] = new String[13][2];
+        for(int fila = 0; fila < 13; fila++){
+            cartas[fila][0] = "As";
+            cartas[fila][1] = "2";
+            cartas[fila][2] = "3";
+            cartas[fila][3] = "4";
+            cartas[fila][4] = "5";
+            cartas[fila][5] = "6";
+            cartas[fila][6] = "7";
+            cartas[fila][7] = "8";
+            cartas[fila][8] = "9";
+            cartas[fila][9] = "10";
+            cartas[fila][10] = "Jota";
+            cartas[fila][11] = "Quina";
+            cartas[fila][12] = "Kaiser";
+            }
+        }
+
+    public static void agregarCartas(String nombreCarta, String puntaje){
+
+    }
+    public static void inicializarCartasJuego(){
+
+    }
+    public static void obtenerCartas(){
+
+    }
+    public static void jugar(){
+
+    }
+}
